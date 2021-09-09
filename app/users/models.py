f22e9b60c9b85cdb2fad9529e06d61e0341f07d4
pavelhardzei <pavelgordei11@gmail.com>
2021-09-09 12:57:34 +0300

    added custom user model

--- app/users/models.py
+++ app/users/models.py
@@ -1,3 +1,60 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.utils import timezone
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, name, date_of_birth, password, **extra_fields):
+        values = [email, name, date_of_birth]
+        field_value_map = dict(zip(self.model.REQUIRED_FIELDS, values))
+        for field_name, value in field_value_map.items():
+            if not value:
+                raise ValueError(f'The {field_name} value must be set')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name,
+            **extra_fields
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, name, date_of_birth, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, name, password, date_of_birth, **extra_fields)
+
+    def create_superuser(self, email, name, date_of_birth, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self._create_user(email, name, date_of_birth, password, **extra_fields)
+
+
+class UserAccount(AbstractBaseUser):
+    email = models.EmailField(unique=True)
+    name = models.CharField(max_length=150)
+    date_of_birth = models.DateField(blank=True, null=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
+    last_login = models.DateTimeField(null=True)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name', 'date_of_birth']
+
+    def get_full_name(self):
+        return self.name
+
+    def __repr__(self):
+        return self.email
