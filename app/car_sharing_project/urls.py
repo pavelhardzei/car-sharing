ebf53085f42828d127d894c43758b1e047636017
pavelhardzei <pavelgordei11@gmail.com>
2021-09-15 12:43:55 +0300

    updated urls

--- app/car_sharing_project/urls.py
+++ app/car_sharing_project/urls.py
@@ -21,5 +21,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', include('base_app.urls')),
     path('', include('users.urls')),
-    path('admin/', admin.site.urls)
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- app/users/urls.py
+++ app/users/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('signup/', views.UserRegister.as_view(), name='signup'),
     path('users/', views.UserList.as_view(), name='users'),
     path('users/<int:pk>/', views.UserDetail.as_view(), name='user_detail')
 ]
