207ac1c6db55d094fdd585c812f623e3a77666e4
pavelhardzei <pavelgordei11@gmail.com>
2021-09-15 12:43:22 +0300

    update views

--- app/users/views.py
+++ app/users/views.py
@@ -1,13 +1,22 @@
 from .models import UserAccount
 from .serializers import UserSerializer
 from rest_framework import generics
+from rest_framework import permissions
+from .permissions import IsAdminOrOwner
+
+
+class UserRegister(generics.CreateAPIView):
+    queryset = UserAccount.objects.all()
+    serializer_class = UserSerializer
 
 
 class UserList(generics.ListAPIView):
     queryset = UserAccount.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAdminUser]
 
 
-class UserDetail(generics.RetrieveAPIView):
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = UserAccount.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAdminOrOwner]
