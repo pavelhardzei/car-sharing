aacf451bab73b43f3bbdb6c908d1920cbb36914b
pavelhardzei <pavelgordei11@gmail.com>
2021-09-15 12:41:48 +0300

    update serializer

--- app/users/serializers.py
+++ app/users/serializers.py
@@ -5,4 +5,17 @@ from .models import UserAccount
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserAccount
-        fields = ['id', 'email', 'name', 'date_of_birth']
+        fields = ['id', 'email', 'name', 'date_of_birth', 'password']
+
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'required': True
+            }
+        }
+
+    def create(self, validated_data):
+        user = UserAccount(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
