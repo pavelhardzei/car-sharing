ea3f5d013efc628fc0b82657c6f56f4dded721cb
pavelhardzei <pavelgordei11@gmail.com>
2021-09-16 12:12:35 +0300

    added obtain_auth_token

--- app/car_sharing_project/urls.py
+++ app/car_sharing_project/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
     path('', include('base_app.urls')),
     path('', include('users.urls')),
     path('admin/', admin.site.urls),
+    path('auth/', obtain_auth_token),
     path('api-auth/', include('rest_framework.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
