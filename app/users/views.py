22dc9da65183e23b0d70c5a8858b533089ad8380
pavelhardzei <pavelgordei11@gmail.com>
2021-09-16 12:54:09 +0300

    added auth class

--- app/users/views.py
+++ app/users/views.py
@@ -3,6 +3,7 @@ from .serializers import UserSerializer
 from rest_framework import generics
 from rest_framework import permissions
 from .permissions import IsAdminOrOwner
+from rest_framework.authentication import TokenAuthentication
 
 
 class UserRegister(generics.CreateAPIView):
@@ -14,9 +15,11 @@ class UserList(generics.ListAPIView):
     queryset = UserAccount.objects.all()
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAdminUser]
+    authentication_classes = (TokenAuthentication, )
 
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = UserAccount.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAdminOrOwner]
+    authentication_classes = (TokenAuthentication, )
