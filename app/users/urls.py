3b436425fa5a7386e1c450bc6dcc55d1743bb9f4
pavelhardzei <pavelgordei11@gmail.com>
2021-09-17 13:44:58 +0300

    updated urls

--- app/users/urls.py
+++ app/users/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('signup/', views.UserRegister.as_view(), name='signup'),
     path('users/', views.UserList.as_view(), name='users'),
-    path('users/<int:pk>/', views.UserDetail.as_view(), name='user_detail')
+    path('users/<int:pk>/', views.UserDetail.as_view(), name='user_detail'),
+    path('auth/', views.AuthToken.as_view(), name='get_token')
 ]
