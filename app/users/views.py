dbe49808e4463a7a70f7a9b2c2abd314fa089a5b
pavelhardzei <pavelgordei11@gmail.com>
2021-09-17 13:43:46 +0300

    added custom ObtainAuthToken

--- app/users/views.py
+++ app/users/views.py
@@ -1,9 +1,12 @@
 from .models import UserAccount
-from .serializers import UserSerializer
+from .serializers import UserSerializer, TokenSerializer
 from rest_framework import generics
 from rest_framework import permissions
 from .permissions import IsAdminOrOwner
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
 
 
 class UserRegister(generics.CreateAPIView):
@@ -23,3 +26,17 @@ class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = UserSerializer
     permission_classes = [IsAdminOrOwner]
     authentication_classes = (TokenAuthentication, )
+
+
+class AuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = TokenSerializer(data=request.data, context={'request': request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'email': user.email
+        })
