3294c41a6d03a99bbf84ac88d695d854047eb255
pavelhardzei <pavelgordei11@gmail.com>
2021-09-30 13:07:25 +0300

    added car model

--- app/cars/admin.py
+++ app/cars/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, Car, CarInfo
 
-# Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Car)
+admin.site.register(CarInfo)
--- app/cars/models.py
+++ app/cars/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    day_fare = models.FloatField()
+    evening_fare = models.FloatField()
+    parking_price = models.FloatField()
+    reservation_price = models.FloatField()
+
+
+class Car(models.Model):
+    brand = models.CharField(max_length=50)
+    register_number = models.CharField(max_length=8)
+    color = models.CharField(max_length=30)
+    year = models.IntegerField()
+    weight = models.IntegerField()
+    mileage = models.IntegerField()
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class CarInfo(models.Model):
+    car_id = models.OneToOneField(Car, on_delete=models.CASCADE, primary_key=True)
+    longitude = models.DecimalField(max_digits=11, decimal_places=8)
+    latitude = models.DecimalField(max_digits=10, decimal_places=8)
+    petrol_level = models.IntegerField()
+    status = models.CharField(max_length=50)
