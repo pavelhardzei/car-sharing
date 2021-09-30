9bbd8dd2e513bdfdb99099e8bea69f2b094f0d82
pavelhardzei <pavelgordei11@gmail.com>
2021-09-30 11:57:05 +0300

    added middleware

--- app/car_sharing_project/settings.py
+++ app/car_sharing_project/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'base_app.middleware.ErrorHandler'
 ]
 
 ROOT_URLCONF = 'car_sharing_project.urls'
