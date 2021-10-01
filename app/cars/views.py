a79fdc2578d87704541f0a21d7a4318627eb2697
pavelhardzei <pavelgordei11@gmail.com>
2021-10-01 12:16:22 +0300

    added views

--- app/cars/views.py
+++ app/cars/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Category, Car, CarInfo
+from .serializers import CategorySerializer, CarSerializer, CarInfoSerializer
 
-# Create your views here.
+
+class CategoryList(generics.ListCreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CarList(generics.ListCreateAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarSerializer
+
+
+class CarInfoList(generics.ListCreateAPIView):
+    queryset = CarInfo.objects.all()
+    serializer_class = CarInfoSerializer
