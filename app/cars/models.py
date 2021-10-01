6f57e458898f575cc55319afd8cc8715fabe8ce8
pavelhardzei <pavelgordei11@gmail.com>
2021-10-01 12:11:23 +0300

    update car model

--- app/cars/models.py
+++ app/cars/models.py
@@ -2,21 +2,27 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50, unique=True)
     day_fare = models.FloatField()
     evening_fare = models.FloatField()
     parking_price = models.FloatField()
     reservation_price = models.FloatField()
 
+    def __str__(self):
+        return self.name
+
 
 class Car(models.Model):
     brand = models.CharField(max_length=50)
-    register_number = models.CharField(max_length=8)
+    register_number = models.CharField(max_length=8, unique=True)
     color = models.CharField(max_length=30)
     year = models.IntegerField()
     weight = models.IntegerField()
     mileage = models.IntegerField()
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.register_number
 
 
 class CarInfo(models.Model):
@@ -25,3 +31,6 @@ class CarInfo(models.Model):
     latitude = models.DecimalField(max_digits=10, decimal_places=8)
     petrol_level = models.IntegerField()
     status = models.CharField(max_length=50)
+
+    def __str__(self):
+        return str(self.car_id)
