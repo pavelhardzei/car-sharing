5dad5b43944a54bc314821b995ec507df7099af3
pavelhardzei <pavelgordei11@gmail.com>
2021-10-01 12:56:32 +0300

    update views

--- app/cars/views.py
+++ app/cars/views.py
@@ -1,18 +1,26 @@
-from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework import permissions
+from rest_framework.authentication import TokenAuthentication
 from .models import Category, Car, CarInfo
 from .serializers import CategorySerializer, CarSerializer, CarInfoSerializer
 
 
-class CategoryList(generics.ListCreateAPIView):
+class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    permission_classes = (permissions.IsAdminUser, )
+    authentication_classes = (TokenAuthentication, )
 
 
-class CarList(generics.ListCreateAPIView):
+class CarViewSet(viewsets.ModelViewSet):
     queryset = Car.objects.all()
     serializer_class = CarSerializer
+    permission_classes = (permissions.IsAdminUser, )
+    authentication_classes = (TokenAuthentication, )
 
 
-class CarInfoList(generics.ListCreateAPIView):
+class CarInfoViewSet(viewsets.ModelViewSet):
     queryset = CarInfo.objects.all()
     serializer_class = CarInfoSerializer
+    permission_classes = (permissions.IsAdminUser, )
+    authentication_classes = (TokenAuthentication, )
