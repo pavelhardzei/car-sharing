959b811280b87903069ad960f6ce2e3cc03c2b91
pavelhardzei <pavelgordei11@gmail.com>
2021-10-05 14:09:21 +0300

    added views

--- app/trips/views.py
+++ app/trips/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework import viewsets
+from .models import Trip, TripState, TripEvent
+from .serializers import TripSerializer, TripStateSerializer, TripEventSerializer
 
-# Create your views here.
+
+class TripViewSet(viewsets.ModelViewSet):
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
+    permission_classes = (permissions.IsAdminUser, )
+
+
+class TripStateViewSet(viewsets.ModelViewSet):
+    queryset = TripState.objects.all()
+    serializer_class = TripStateSerializer
+    permission_classes = (permissions.IsAdminUser, )
+
+
+class TripEventViewSet(viewsets.ModelViewSet):
+    queryset = TripEvent.objects.all()
+    serializer_class = TripEventSerializer
+    permission_classes = (permissions.IsAdminUser, )
