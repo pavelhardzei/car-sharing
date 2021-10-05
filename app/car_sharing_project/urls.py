9a016dd86255a009859d3e4c5d27732000daeafc
pavelhardzei <pavelgordei11@gmail.com>
2021-10-05 14:11:28 +0300

    update urls

--- app/car_sharing_project/urls.py
+++ app/car_sharing_project/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('', include('base_app.urls')),
     path('', include('users.urls')),
     path('', include('cars.urls')),
+    path('', include('trips.urls')),
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ app/trips/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register('trip', views.TripViewSet)
+router.register('tripstate', views.TripStateViewSet)
+router.register('tripevent', views.TripEventViewSet)
+
+urlpatterns = [
+    path('', include(router.urls))
+]
