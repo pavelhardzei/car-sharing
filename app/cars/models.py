9e073e6e205c68e218f1342c01045c5c0da36405
pavelhardzei <pavelgordei11@gmail.com>
2021-10-06 14:18:00 +0300

    added Car and CarInfo field validation

--- app/cars/models.py
+++ app/cars/models.py
@@ -21,6 +21,16 @@ class Car(models.Model):
     mileage = models.IntegerField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category')
 
+    def clean(self):
+        if self.weight <= 0:
+            raise Exception('Weight must be positive')
+        if self.mileage < 0:
+            raise Exception('Mileage cannot be negative')
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
+
     def __str__(self):
         return self.register_number
 
@@ -31,6 +41,18 @@ class CarInfo(models.Model):
     latitude = models.DecimalField(max_digits=10, decimal_places=8)
     petrol_level = models.IntegerField()
     status = models.CharField(max_length=50)
+
+    def clean(self):
+        if self.petrol_level < 0 or self.petrol_level > 100:
+            raise Exception('Petrol level must be in range(0, 100)')
+        if self.longitude < -180 or self.longitude > 180:
+            raise Exception('Longitude must be in range(-180, 180)')
+        if self.latitude < -90 or self.latitude > 90:
+            raise Exception('Latitude must be in range(-90, 90)')
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        return super().save(*args, **kwargs)
 
     def __str__(self):
         return str(self.car)
