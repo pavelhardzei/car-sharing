3f006e296b752fce17798d8138974e861e847397
pavelhardzei <pavelgordei11@gmail.com>
2021-10-07 10:56:21 +0300

    used constants

--- app/cars/tests.py
+++ app/cars/tests.py
@@ -16,22 +16,22 @@ class UsersManagersTests(TestCase):
                                             reservation_price=3)
         self.assertEqual(category1.id, 1)
         self.assertEqual(category1.name, 'Economy')
-        car1 = Car.objects.create(brand='Mercedes', register_number='1111KK-1', color='dark blue', year=2020,
+        car1 = Car.objects.create(brand='Mercedes', register_number='1111KK-1', color=Car.Color.blue, year=2020,
                                   weight=600, mileage=1000, category=category1)
-        car2 = Car.objects.create(brand='BMW', register_number='2222KK-2', color='black', year=2021, weight=700,
+        car2 = Car.objects.create(brand='BMW', register_number='2222KK-2', color=Car.Color.black, year=2021, weight=700,
                                   mileage=3000, category=category2)
         self.assertEqual(car1.category.id, 1)
         self.assertEqual(car2.category.id, 2)
         car_info1 = CarInfo.objects.create(car=car1, longitude='33.345612', latitude='34.124533', petrol_level=90,
-                                           status='FREE')
+                                           status=CarInfo.Status.available)
         car_info2 = CarInfo.objects.create(car=car2, longitude='35.453312', latitude='36.442213', petrol_level=80,
-                                           status='FREE')
+                                           status=CarInfo.Status.available)
         self.assertEqual(car_info1.car.register_number, '1111KK-1')
         self.assertEqual(car_info2.car.register_number, '2222KK-2')
 
         with self.assertRaises(Exception):
             CarInfo.objects.create(car=car1, longitude='33.345612', latitude='34.124533', petrol_level=90,
-                                   status='FREE')
+                                   status=CarInfo.Status.available)
 
     def test_permissions(self):
         client = APIClient()
@@ -58,21 +58,21 @@ class UsersManagersTests(TestCase):
 
         category = Category.objects.all().get(name='Economy')
         response = client.post(reverse('car-list'),
-                               {'brand': 'Mercedes', 'register_number': '7777KC-7', 'color': 'brawn', 'year': 2020,
+                               {'brand': 'Mercedes', 'register_number': '7777KC-7', 'color': Car.Color.brawn, 'year': 2020,
                                 'weight': 800, 'mileage': 1500, 'category': category.id})
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         car = Car.objects.all().get(register_number='7777KC-7')
         response = client.post(reverse('car-list'),
-                               {'brand': 'Mercedes', 'register_number': '7777KC-7', 'color': 'brawn', 'year': 2020,
+                               {'brand': 'Mercedes', 'register_number': '7777KC-7', 'color': Car.Color.brawn, 'year': 2020,
                                 'weight': 800, 'mileage': 1500, 'category': 100})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
         response = client.post(reverse('carinfo-list'),
-                               {'car': car.id, 'longitude': '33.345612', 'latitude': '34.124533', 'petrol_level': 90,
-                                'status': 'FREE'})
+                               {'car': car.id, 'longitude': 33.345612, 'latitude': 34.124533, 'petrol_level': 90,
+                                'status': CarInfo.Status.broken})
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
         response = client.post(reverse('carinfo-list'),
                                {'car': car.id, 'longitude': '33.345612', 'latitude': '34.124533', 'petrol_level': 90,
-                                'status': 'FREE'})
+                                'status': CarInfo.Status.available})
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
