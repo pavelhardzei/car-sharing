3a497de98b585232eecb97d256090c70ed5b2216
pavelhardzei <pavelgordei11@gmail.com>
2021-10-08 10:29:47 +0300

    fixed

--- app/trips/models.py
+++ app/trips/models.py
@@ -55,7 +55,7 @@ class TripState(models.Model):
         return super().save(*args, **kwargs)
 
     def __str__(self):
-        return self.trip
+        return str(self.trip)
 
 
 class TripEvent(models.Model):
