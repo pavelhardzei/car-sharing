9f05dfb0f78b754366d328c4853c5c4aca13f08a
pavelhardzei <pavelgordei11@gmail.com>
2021-10-21 15:04:28 +0300

    refactored if statements

--- app/trips/views.py
+++ app/trips/views.py
@@ -87,31 +87,25 @@ class TripManagement(views.APIView):
         action = request.data['action']
         car = get_car(car_id)
 
-        if action == TripEvent.Event.booking:
-            trip = self.create_trip(request.user, car, TripEvent.Event.booking)
-            trip_ser = TripSerializer(trip)
-
-            return Response(trip_ser.data, status=status.HTTP_201_CREATED)
-        elif action == TripEvent.Event.landing:
-            current_trip = get_current_trip(user=request.user.id)
-            if current_trip:
-                if current_trip.events.first().event != TripEvent.Event.booking or current_trip.events.count() != 1:
-                    raise ValidationError({'error_message': 'Your trip already exists'})
-
-                event = TripEvent.objects.create(trip=current_trip, event=TripEvent.Event.landing)
-                current_trip.events.add(event)
-                current_trip.reservation_time = (event.timestamp - current_trip.events.first().timestamp).total_seconds() // 60
-                current_trip.save()
-                trip_ser = TripSerializer(current_trip)
-
-                return Response(trip_ser.data, status=status.HTTP_200_OK)
-            else:
-                trip = self.create_trip(request.user, car, TripEvent.Event.landing)
-                trip_ser = TripSerializer(trip)
-
-                return Response(trip_ser.data, status=status.HTTP_201_CREATED)
-        else:
+        if action not in (TripEvent.Event.booking, TripEvent.Event.landing):
             raise ValidationError({'error_message': 'Invalid action'})
+
+        current_trip = get_current_trip(user=request.user.id)
+        if current_trip is None:
+            trip = self.create_trip(request.user, car, action)
+            trip_ser = TripSerializer(trip)
+            return Response(trip_ser.data, status=status.HTTP_201_CREATED)
+
+        if action == TripEvent.Event.booking or current_trip.events.first().event != TripEvent.Event.booking or current_trip.events.count() != 1:
+            raise ValidationError({'error_message': 'Your trip already exists'})
+
+        event = TripEvent.objects.create(trip=current_trip, event=TripEvent.Event.landing)
+        current_trip.events.add(event)
+        current_trip.reservation_time = (event.timestamp - current_trip.events.first().timestamp).total_seconds() // 60
+        current_trip.save()
+        trip_ser = TripSerializer(current_trip)
+
+        return Response(trip_ser.data)
 
 
 class TripMaintenance(views.APIView):
@@ -132,55 +126,44 @@ class TripMaintenance(views.APIView):
         trip = get_current_trip(car=request.data['car'])
         if trip is None:
             return Response({'message': 'Current trip doesn\'t exist'})
+
         car = trip.car
         event = request.data['event']
         credentials = request.data['credentials']
 
         filtered = {k: v for k, v in request.data.items() if v}
+
         car_info_ser = CarInfoSerializer(car.car_info, data=filtered, partial=True)
-        if car_info_ser.is_valid():
-            car_info_ser.save()
-        else:
-            raise ValidationError(car_info_ser.errors)
+        car_info_ser.is_valid(raise_exception=True)
+        car_info_ser.save()
 
         trip_ser = TripSerializer(trip, data=filtered, partial=True)
-        if trip_ser.is_valid():
-            trip_ser.save()
-        else:
-            raise ValidationError(trip_ser.errors)
+        trip_ser.is_valid(raise_exception=True)
+        trip_ser.save()
 
-        if event in TripEvent.Event.values:
-            if event in (TripEvent.Event.booking, TripEvent.Event.landing, TripEvent.Event.end):
-                raise ValidationError({'error_message': f'Event \'{event}\' must be set in another place'})
-
-            trip_event = TripEvent(trip=trip, event=event)
-            previous_event = trip.events.last()
-
-            if event != TripEvent.Event.end_parking and previous_event.event == TripEvent.Event.parking:
-                raise ValidationError({'error_message': f'Event \'{event}\' cannot occur after \'{previous_event.event}\''})
-            if event == TripEvent.Event.end_parking:
-                if previous_event.event != TripEvent.Event.parking:
-                    raise ValidationError({'error_message': f'Event \'{event}\' must occur after \'{TripEvent.Event.parking}\''})
-            if event == TripEvent.Event.fueling:
-                if credentials is None:
-                    raise ValidationError({'error_message': f'Credentials must be transferred when \'{event}\''})
-                car.car_info.petrol_level = 100
-                car.car_info.save()
-            if event == TripEvent.Event.washing and credentials is None:
-                raise ValidationError({'error_message': f'Credentials must be transferred when \'{event}\''})
-
-            if credentials:
-                trip_event.credentials = credentials
-                trip_event.cost = self.pay_by_credentials(credentials)
-                if trip.total_cost:
-                    trip.total_cost += trip_event.cost
-                else:
-                    trip.total_cost = trip_event.cost
-                trip.save()
-            trip_event.save()
-            trip.events.add(trip_event)
-        elif event:
+        if event is None:
+            return Response(trip_ser.data)
+        if event not in TripEvent.Event.values or event in (TripEvent.Event.booking, TripEvent.Event.landing, TripEvent.Event.end):
             raise ValidationError({'error_message': 'Invalid event'})
+
+        trip_event = TripEvent(trip=trip, event=event)
+        previous_event = trip.events.last()
+
+        if event != TripEvent.Event.end_parking and previous_event.event == TripEvent.Event.parking:
+            raise ValidationError({'error_message': f'Event \'{event}\' cannot occur after \'{previous_event.event}\''})
+        if event == TripEvent.Event.end_parking and previous_event.event != TripEvent.Event.parking:
+            raise ValidationError({'error_message': f'Event \'{event}\' must occur after \'{TripEvent.Event.parking}\''})
+        if event in (TripEvent.Event.fueling, TripEvent.Event.washing) and credentials is None:
+            raise ValidationError({'error_message': f'Credentials must be transferred when \'{event}\''})
+
+        if credentials:
+            trip_event.credentials = credentials
+            trip_event.cost = self.pay_by_credentials(credentials)
+            trip.total_cost += trip_event.cost
+            trip.save()
+
+        trip_event.save()
+        trip.events.add(trip_event)
 
         return Response(trip_ser.data)
 
@@ -196,8 +179,7 @@ class TripsHistory(generics.ListAPIView):
 
 def get_total_cost(trip):
     total_cost = 0
-    if trip.total_cost:
-        total_cost += trip.total_cost
+    total_cost += trip.total_cost
     if trip.reservation_time:
         total_cost += trip.reservation_time * trip.state.reservation_price
     current_time = timezone.now()
