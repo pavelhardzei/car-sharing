a4ab542cd75d686efeb8f60e2a7ba385a57c6d09
pavelhardzei <pavelgordei11@gmail.com>
2021-10-25 13:28:40 +0300

    update tests

--- app/users/tests.py
+++ app/users/tests.py
@@ -1,126 +1,63 @@
 from django.contrib.auth import get_user_model
-from django.test import TestCase
-from django.db.utils import IntegrityError
+from django.test import TestCase, RequestFactory
 from django.urls import reverse
-import json
 from rest_framework.test import APIClient
-from rest_framework import status
+from rest_framework import status, permissions
+from .permissions import IsAdminOrOwner
 
 
 class UsersManagersTests(TestCase):
 
-    def test_create_user(self):
-        User = get_user_model()
-        user = User.objects.create_user(email='normal@user.com', name='test', date_of_birth='2002-12-12')
-        self.assertEqual(user.email, 'normal@user.com')
-        self.assertEqual(user.name, 'test')
-        self.assertTrue(user.is_active)
-        self.assertFalse(user.is_staff)
-        self.assertFalse(user.is_superuser)
+    def setUp(self):
+        self.User = get_user_model()
+        self.superuser = self.User.objects.create_superuser(email='super@user.com', name='test', date_of_birth='2002-12-12', password='hello_world')
+        self.normaluser = self.User.objects.create_user(email='normal@user.com', name='test1', date_of_birth='2002-12-12', password='hello_world')
 
-        with self.assertRaises(Exception):
-            User.objects.create_user()
-        with self.assertRaises(IntegrityError):
-            User.objects.create_user(email='normal@user.com', name='test', date_of_birth='2002-12-12')
-        with self.assertRaises(Exception):
-            User.objects.create_user(email='', name='', date_of_birth='')
-        with self.assertRaises(Exception):
-            User.objects.create_user(email='normal2@user.com', name='test', date_of_birth='2012-12-12')
+        self.factory = RequestFactory()
 
-    def test_create_superuser(self):
-        User = get_user_model()
-        admin_user = User.objects.create_superuser(email='super@user.com', name='test', date_of_birth='2000-12-12')
-        self.assertEqual(admin_user.email, 'super@user.com')
-        self.assertTrue(admin_user.is_active)
-        self.assertTrue(admin_user.is_staff)
-        self.assertTrue(admin_user.is_superuser)
-
-        with self.assertRaises(Exception):
-            User.objects.create_superuser(email='super2@user.com', name='test2', date_of_birth='2000-12-12',
-                                          is_superuser=False)
+        self.api_client = APIClient()
+        self.api_client.force_authenticate(user=self.normaluser)
 
     def test_permissions(self):
-        response = self.client.get(reverse('users'))
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+        permission = permissions.IsAdminUser()
 
-        User = get_user_model()
-        superuser = User.objects.create_superuser(email='super@user.com', name='test', date_of_birth='2002-12-12', password='hello_world')
-        user = User.objects.create_user(email='normal@user.com', name='test1', date_of_birth='2002-12-12', password='hello_world')
-        superuser.save()
-        user.save()
+        request = self.factory.get(reverse('users'))
+        request.user = self.superuser
+        self.assertTrue(permission.has_permission(request, None))
+        request.user = self.normaluser
+        self.assertFalse(permission.has_permission(request, None))
 
-        response = self.client.post(reverse('get_token'), {'name': 'test', 'password': '1'})
-        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        permission = IsAdminOrOwner()
+        request = self.factory.get(reverse('user_detail', kwargs={'pk': self.superuser.pk}))
+        request.user = self.normaluser
+        self.assertFalse(permission.has_object_permission(request, None, self.superuser))
 
-        response = self.client.post(reverse('get_token'), {'name': 'test', 'password': 'hello_world'})
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        token = json.loads(response.content)['token']
+        request = self.factory.get(reverse('user_detail', kwargs={'pk': self.normaluser.pk}))
+        request.user = self.superuser
+        self.assertTrue(permission.has_object_permission(request, None, self.normaluser))
 
-        client = APIClient()
-        client.credentials(HTTP_AUTHORIZATION=f'Token {token}')
-        response = client.get(reverse('users'))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        response = client.get(reverse('user_detail', kwargs={'pk': superuser.pk}))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-        response = client.get(reverse('user_detail', kwargs={'pk': user.pk}))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-        response = self.client.post(reverse('get_token'), {'email': 'normal@user.com', 'password': 'hello_world'})
-        token = json.loads(response.content)['token']
-        client.credentials(HTTP_AUTHORIZATION=f'Token {token}')
-        response = client.get(reverse('users'))
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-        response = client.get(reverse('user_detail', kwargs={'pk': superuser.pk}))
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-        response = client.get(reverse('user_detail', kwargs={'pk': 'me'}))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_signup(self):
         response = self.client.post(reverse('users'))
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-        response = self.client.post(reverse('users'), {'email': 'normal@user.com', 'name': 'test',
-                                                       'date_of_birth': '2000-01-01', 'password': 'hello_world'})
+        response = self.client.post(reverse('users'), {'email': 'new@user.com', 'name': 'new_user', 'date_of_birth': '2000-01-01', 'password': 'hello_world'})
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
     def test_update(self):
-        User = get_user_model()
-        superuser = User.objects.create_superuser(email='super@user.com', name='test', date_of_birth='2002-12-12',
-                                                  password='hello_world')
-        user = User.objects.create_user(email='normal@user.com', name='test1', date_of_birth='2002-12-12',
-                                        password='hello_world')
-        superuser.save()
-        user.save()
-
-        response = self.client.post(reverse('get_token'), {'name': 'test', 'password': 'hello_world'})
-        token = json.loads(response.content)['token']
-
-        client = APIClient()
-        client.credentials(HTTP_AUTHORIZATION=f'Token {token}')
-
-        response = client.put(reverse('update_user', kwargs={'pk': user.pk}), data={'email': 'changed@user.com', 'name': 'changed'})
+        response = self.api_client.put(reverse('update_user', kwargs={'pk': self.normaluser.pk}), {'email': 'changed@user.com', 'name': 'changed'})
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-        user = User.objects.get(pk=user.pk)
+        user = self.User.objects.get(pk=self.normaluser.pk)
         self.assertEqual(user.email, 'changed@user.com')
         self.assertEqual(user.name, 'changed')
 
+    def test_change_password(self):
+        response = self.api_client.put(reverse('change_password', kwargs={'pk': 'me'}), {'old_password': 'hello_world',
+                                       'password': 'testing321', 'password_rep': 'testing321'})
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(self.User.objects.get(pk=self.normaluser.pk).check_password('testing321'))
+
     def test_delete(self):
-        User = get_user_model()
-        superuser = User.objects.create_superuser(email='super@user.com', name='test', date_of_birth='2002-12-12',
-                                                  password='hello_world')
-        user = User.objects.create_user(email='normal@user.com', name='test1', date_of_birth='2002-12-12',
-                                        password='hello_world')
-        superuser.save()
-        user.save()
-
-        response = self.client.post(reverse('get_token'), {'name': 'test', 'password': 'hello_world'})
-        token = json.loads(response.content)['token']
-
-        client = APIClient()
-        client.credentials(HTTP_AUTHORIZATION=f'Token {token}')
-
-        response = client.delete(reverse('user_detail', kwargs={'pk': 'me'}))
+        response = self.api_client.delete(reverse('user_detail', kwargs={'pk': 'me'}))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
