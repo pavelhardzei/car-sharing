fe57e68f473d9751d70e7983fb7de32bbd840c55
pavelhardzei <pavelgordei11@gmail.com>
2021-10-29 12:03:49 +0300

    added cars checking

--- app/cars/periodic_tasks.py
+++ app/cars/periodic_tasks.py
@@ -3,5 +3,13 @@ from celery import shared_task
 
 @shared_task
 def periodic_task():
-    with open('testing.txt', 'a') as file:
-        file.write('test')
+    from .models import Car, CarInfo
+    from .serializers import CarSerializer
+
+    cars = Car.objects.select_related('car_info').filter(car_info__status=CarInfo.Status.broken)
+    cars_ser = CarSerializer(cars, many=True)
+    with open('report.txt', 'w') as file:
+        if cars.count():
+            file.write(f'Broken cars: {cars_ser.data}')
+        else:
+            file.write('Everything is ok')
